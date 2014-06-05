31b84592943fb8ee811ef97650ead7eaa6be9072
Arjun <arjun.bali96@gmail.com>
2014-06-04 21:18:55 -0400

    created all classes and methods

--- people.rb
+++ people.rb
@@ -3,19 +3,19 @@ class Person
 		@name = name
 	end
 
-	def greeting(name)
-		puts "Hi, my name is #{name}"
+	def greeting
+		puts "Hi, my name is #{@name}"
 	end
 end
 
 
-class Student
+class Student < Person
 	def learn
 		puts "I get it!"
 	end 
 end
 
-class Instructor
+class Instructor < Person
 	def teach 
 		puts "Everything in Ruby is an Object"
 	end
